ee2244be3dc85a0f95716c33ffb5ddb83766d376
Diogo Holanda <dholanda@palantir.com>
2015-03-13 18:08:20 +0000

    earlier

--- src/main/java/EarlierTimeFinder.java
+++ src/main/java/EarlierTimeFinder.java
@@ -8,13 +8,26 @@ public class EarlierTimeFinder {
 		for(int i=0; i<times.length; i++){
 			times[i] = times[i].trim();
 			int t = 0;
-			for(int j=0; j<times[i].length(); j++){
-				if(times[i].charAt(j)>='0' && times[i].charAt(j)<='9'){
-					t = 10*t+(times[i].charAt(j)-'0');
+			
+			if(times[i].charAt(times[i].length()-1) == 'm'){
+				for(int j=0; j<times[i].length(); j++){
+					if(times[i].charAt(j)>='0' && times[i].charAt(j)<='9'){
+						t = 10*t+(times[i].charAt(j)-'0');
+					}
+					else break;
 				}
-				else break;
+				if(times[i].charAt(times[i].length()-2) == 'p') t+=12;
 			}
-			if(times[i].charAt(times[i].length()-2) == 'p') t+=12;
+			else{
+				for(int j=0; j<times[i].length(); j++){
+					if(times[i].charAt(j)>='0' && times[i].charAt(j)<='9'){
+						if(j<2) t = 10*t+(times[i].charAt(j)-'0');
+					}
+					else if(times[i].charAt(j)=='.'){
+						break;
+					}
+				}
+			}
 			
 			if(t<earliest){
 				answer = times[i];
@@ -25,6 +38,6 @@ public class EarlierTimeFinder {
 	}
 	
 	public static void main(String[] args){
-		System.out.println(findEarliest(" 9pm, 7am"));
+		System.out.println(findEarliest("0500, 8am, 10.00, 13.00, 6pm, 10am"));
 	}
 }
